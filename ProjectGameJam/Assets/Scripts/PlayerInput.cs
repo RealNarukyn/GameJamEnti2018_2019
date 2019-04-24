665632716174ba7422dd17676b6cf8d356eff3b9
Alejandro <alexmusu99@gmail.com>
2019-04-24 13:43:44 +0200

    Added Linterna

---
+++ ProjectGameJam/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    public SpriteRenderer black_background; 
+    public SpriteRenderer linterna; 
+
+	// Use this for initialization
+	void Start () {
+        //black_background = GameObject.Find("BlackBackground").GetComponent<SpriteRenderer>();
+        //linterna = GameObject.Find("Linterna").GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            black_background.enabled = false;
+            linterna.enabled = true;
+
+        }
+        else {
+            black_background.enabled = true;
+            linterna.enabled = false;
+        }
+    }
+
+
+
+
+}
