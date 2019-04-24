e2826c17a910886dec22e77cd342a966855f04de
Bernat <bernat.grifol@enti.cat>
2019-04-24 22:47:55 +0200

    Added Menu 2

--- ProjectGameJam/Assets/Scripts/Menu.cs
+++ ProjectGameJam/Assets/Scripts/Menu.cs
@@ -3,9 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Menu : MonoBehaviour {
-   
+
+    public Canvas Canvas_Init;
+    public Canvas Canvas_Options;
+    public Canvas Canvas_Credits;
+
+    void Start()
+    {
+        Canvas_Init.gameObject.SetActive(true);
+        Canvas_Options.gameObject.SetActive(false);
+        Canvas_Credits.gameObject.SetActive(false);
+    }
+
+    public void Options()
+
+    {
+        Canvas_Init.gameObject.SetActive(false);
+        Canvas_Options.gameObject.SetActive(true);
+    }
+
+    public void Creditos()
+    {
+        Canvas_Init.gameObject.SetActive(false);
+        Canvas_Credits.gameObject.SetActive(true);
+    }
+
+    public void Back_Options()
+    {
+        Canvas_Init.gameObject.SetActive(true);
+        Canvas_Options.gameObject.SetActive(false);
+    }
+
+    public void Back_Creditos()
+    {
+        Canvas_Init.gameObject.SetActive(true);
+        Canvas_Credits.gameObject.SetActive(false);
+    }
+
     public void doExitGame()
     {
         Application.Quit();
     }
+
 }
